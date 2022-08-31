1ec879d2101a3399e61b18e2e83a60b1273196c2
Kamil <isaevkamil029@mail.ru>
2022-08-31 21:22:43 +0300

    sc

--- js/script.js
+++ js/script.js
@@ -243,3 +243,59 @@ const sumOfTwo = (arr, target) => {
 };
 
 console.log(sumOfTwo( [2, 7, 11, 15], 26 ));
+
+
+
+// СУММА ТРЕХ
+const Array = [-1, 0, 1, 2, -1, -4];
+
+const threeSum = function (nums, target = 0) {
+    const result = [];
+
+    if(nums.length < 3) {
+        return result;
+    }
+    nums = nums.sort((a,b) => a - b);
+
+    for(let i=0; i < nums.length - 2; i++) {
+        if (nums[i] > target) {
+            break;
+        }
+
+        if(i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let j = i + 1;
+        let k = nums.length - 1
+
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            
+            if(sum === target) {
+                result.push([nums[i], nums[j], nums[k]]);
+
+                while (nums[j] === nums[j + 1]) j++;
+                while (nums[k] === nums[k - 1]) k--;
+
+                j++;
+                k--;
+                continue;
+            }
+            if(sum < target) {
+                j++;
+                continue;
+            }
+            if(sum > target) {
+                k--;
+                continue;
+            }
+        }
+    }
+    return result;
+};
+
+console.log(threeSum(Array));
+
+
+
